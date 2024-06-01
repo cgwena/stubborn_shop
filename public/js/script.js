abbf3ad89d4c9258bfefa73fa261752b83dc8293
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-06-01 14:34:30 +0200

    css fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,25 @@
-function priceChoice(value) {
-    fetch(`/filter-products?priceRange=${value}`)
-        .then(response => response.json())
-        .then(data => {
-            const productsContainer = document.querySelector('.products');
-            productsContainer.innerHTML = '';
+// function priceChoice(value) {
+//     fetch(`/filter-products?priceRange=${value}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             const productsContainer = document.querySelector('.products');
+//             productsContainer.innerHTML = '';
 
-            data.forEach(product => {
-                const productDiv = document.createElement('div');
-                productDiv.classList.add('products_details');
+//             data.forEach(product => {
+//                 const productDiv = document.createElement('div');
+//                 productDiv.classList.add('products_details');
 
-                productDiv.innerHTML = `
-                    <img src="/images/products/${product.imageName}">
-                    <h2>${product.name}</h2>
-                    <h3>${product.price} €</h3>
-                    <a href="/product/${product.id}">
-                        <button class='btn btn-primary w-100'>Voir</button>
-                    </a>
-                `;
+//                 productDiv.innerHTML = `
+//                     <img src="/images/products/${product.imageName}">
+//                     <h2>${product.name}</h2>
+//                     <h3>${product.price} €</h3>
+//                     <a href="/product/${product.id}">
+//                         <button class='btn btn-primary w-100'>Voir</button>
+//                     </a>
+//                 `;
 
-                productsContainer.appendChild(productDiv);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-function checkoutSession() {
-    
-}
+//                 productsContainer.appendChild(productDiv);
+//             });
+//         })
+//         .catch(error => console.error('Error:', error));
+// }
